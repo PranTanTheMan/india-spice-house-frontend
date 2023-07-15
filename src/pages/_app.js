e732d127d5f0179eb6d7751ac9bf74bd2ab50bbf
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2023-07-14 21:22:16 -0500

    setup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </>
+  );
 }

e5e26083ae0e3853384b07d48cdf22f40efde9ed
Pranith Molakalapalli <46548127+PranTanTheMan@users.noreply.github.com>
2024-05-09 14:18:58 -0500

    added chatbot

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,6 @@
 import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import Script from "next/script";
 import Layout from "@/components/layout";
 
 export default function App({ Component, pageProps }) {
@@ -9,13 +10,23 @@ export default function App({ Component, pageProps }) {
         <Layout>
           <Component {...pageProps} />
 
-          <script
-            src="//code.tidio.co/zbbipf0hzh3epsuhiuiklvcz2ylwowis.js"
-            async
-          ></script>
+
+
+          <Script id="aichatbot-script" strategy="afterInteractive">
+            {`!function(w, d, s, ...args) {
+    var div = d.createElement('div');
+    div.id = 'aichatbot';
+    d.body.appendChild(div);
+    w.chatbotConfig = args;
+    var f = d.getElementsByTagName(s)[0], j = d.createElement(s);
+    j.defer = true;
+    j.type = 'module';
+    j.src = 'https://aichatbot.sendbird.com/index.js';
+    f.parentNode.insertBefore(j, f);
+  }(window, document, 'script', 'AF659653-E8C8-4897-999F-05C70E7B55D8', 'onboarding_bot');`}
+          </Script>
         </Layout>
       </ChakraProvider>
-      {/* ✅ */}
     </>
   );
 }
